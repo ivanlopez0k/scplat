3de41ee0c3d34cff938421876be2d748d8aa0eb1
Ivan <ivanlopezokk@gmail.com>
2026-03-18 18:17:26 -0300

    bd connect

--- sc-back/index.js
+++ sc-back/index.js
@@ -1,7 +1,56 @@
+require('dotenv').config();
+const models = require('./models')
+const logger = require('./utils/winston.logger');
 const express = require('express');
+const helmet = require('helmet');
+const session = require('express-session');
+const cors = require('cors');
+const config = require('./config/config');
+const validateEnv = require('./middlewares/validateEnv');
 
 const app = express();
-const PORT = 3000;
-app.listen(PORT, ()=>{
-    console.log('sc-plat back working at ', PORT)
+
+validateEnv.validate();
+
+// Body parsers
+app.use(express.json({ limit: '10kb' }));
+app.use(express.urlencoded({ extended: true }));
+
+// Security
+app.use(helmet());
+
+// CORS
+app.use(cors({ credentials: true, origin: true }));
+
+// Sessions
+if (config.environment === 'production') {
+  app.set('trust proxy', 1);
+}
+
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: true,
+  cookie: {
+    sameSite: 'strict',
+    secure: config.environment === 'production',
+  },
+}));
+
+// DB connection
+models.sequelize.authenticate()
+  .then(() => {
+    logger.api.debug('Conexión con la Base de Datos: EXITOSA');
+  })
+  .catch((err) => {
+    logger.api.error('Conexión con la Base de Datos: FALLIDA');
+    logger.api.error(err);
 });
+
+// Server
+const PORT = process.env.PORT;
+app.listen(PORT, () => {
+  console.log('sc-plat back working at ', PORT)
+});
+
+module.exports = app;
