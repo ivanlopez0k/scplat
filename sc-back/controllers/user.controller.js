5e995cd1468490eb012f7180dba0493387970c7d
Ivan <ivanlopezokk@gmail.com>
2026-07-28 17:50:02 -0300

    cors4

--- sc-back/controllers/user.controller.js
+++ sc-back/controllers/user.controller.js
@@ -22,7 +22,7 @@ async function login(req, res){
         res.cookie('token', token, {
             httpOnly: true,
             secure: process.env.ENVIRONMENT === 'production',
-            sameSite: 'strict',
+            sameSite: process.env.ENVIRONMENT === 'production' ? 'none' : 'lax',
             maxAge: 8 * 60 * 60 * 1000, // 8 hours
             path: '/'
         });
